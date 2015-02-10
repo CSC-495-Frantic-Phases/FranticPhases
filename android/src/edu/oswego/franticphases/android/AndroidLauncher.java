265a1aa17aeda390ab869a61b07193fd08b66c3f
kmae26 <kellymaestri@Kellys-MacBook-Pro.local>
2015-02-10 13:12:11 -0500

    Adding project

---
+++ android/src/edu/oswego/franticphases/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package edu.oswego.franticphases.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.oswego.franticphases.FranticPhases;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FranticPhases(), config);
+	}
+}
---
+++ desktop/src/edu/oswego/franticphases/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.oswego.franticphases.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.oswego.franticphases.FranticPhases;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FranticPhases(), config);
+	}
+}
